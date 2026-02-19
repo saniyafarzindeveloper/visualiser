c8389ee247d22edccafad7aeb30f5f4a1ebe360a
saniyafarzindeveloper <developerfarzin@gmail.com>
2026-02-19 22:09:34 +0530

    completed hero section & dummy upload section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -13,7 +15,45 @@ export default function Home() {
   return(
     <div className="home">
     <Navbar />
-     
+     <section className="hero">
+      <div className="announce">
+        <div className="dot">
+          <div className="pulse">
+
+          </div>
+        </div>
+
+        <p>Introducing Visualizer</p>
+      </div>
+      <h1>Visualize your 2D plans to 3D in a matter of minutes</h1>
+      <p className="subtitle">
+        We help you visualize, render & ship architecture - powered by AI
+      </p>
+
+      <div className="actions">
+        <a href="#upload" className="cta">
+          Start building <ArrowRight className="icon" />
+        </a>
+
+        <Button variant="outline" size="lg" className="demo">
+          Watch Demo
+        </Button>
+      </div>
+
+      <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+              <div className="upload-head">
+                  <div className="upload-icon">
+                    <Layers className="icon" />
+                  </div>
+                  <h3>Upload your floor plan</h3>
+                  <p>**Supports only JPG, PNG formats upto 10MB</p>
+              </div>
+              <p>Upload Images</p>
+          </div>
+      </div>
+     </section>
      </div>
     );
 }
