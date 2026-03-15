a643f5f3d3f668408ab926d20423e51c2c0b0050
saniyafarzindeveloper <developerfarzin@gmail.com>
2026-03-15 21:45:29 +0530

    working on ai visualizer

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,8 +1,34 @@
-import { useLocation } from "react-router"
+import { generate3DView } from "lib/ai.action";
+import { useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router"
 
 const VisualizerId = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const {initialImage , name} = location.state || {};
+  const {initialImage, initialRender, name} = location.state || {};
+  const hasInitiallyGenerated = useRef(false);
+
+  //states
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(initialRender);
+
+  const handleBack = () => navigate('/');
+
+  const runGeneration = async() =>{
+    if(!initialImage) return;
+
+    try {
+      setIsProcessing(true);
+      const result = await generate3DView({sourceImage: initialImage});
+      if(result.renderedImage){
+        setCurrentImage(result.renderedImage);
+      }
+    } catch (error) {
+      console.log('Failed to generate', error);
+    }finally{
+      setIsProcessing(false);
+    }
+  } 
 
   return (
     <section>
--- lib/ai.action.ts
+++ lib/ai.action.ts
@@ -1,5 +1,5 @@
 import puter from "@heyputer/puter.js";
-import {ROOMIFY_RENDER_PROMPT} from '../lib/constants'
+import { ROOMIFY_RENDER_PROMPT } from "../lib/constants";
 
 export const fetchAsDataUrl = async (url: string): Promise<string> => {
   const response = await fetch(url);
@@ -18,22 +18,33 @@ export const fetchAsDataUrl = async (url: string): Promise<string> => {
   });
 };
 
-
-export const generate3DView = async ({sourceImage} : Generate3DViewParams) => {
-    const dataUrl = sourceImage.startsWith('data:')
+export const generate3DView = async ({ sourceImage }: Generate3DViewParams) => {
+  const dataUrl = sourceImage.startsWith("data:")
     ? sourceImage
     : await fetchAsDataUrl(sourceImage);
 
-    const base64Data = dataUrl.split(',')[1];
-    const mimeType = dataUrl.split(';')[0].split(':')[1];
-    if(!mimeType || !base64Data){
-        throw new Error('Invalid source image payload');
-        const response = await puter.ai.txt2img(ROOMIFY_RENDER_PROMPT,{
-            provider: 'gemini',
-            model: 'gemini-2.5-flash-image-preview',
-            input_image: base64Data,
-            input_image_mime_type: mimeType,
-            ration: {w: 1024, h: 1024}
-        });
-    }
-}
+  const base64Data = dataUrl.split(",")[1];
+  const mimeType = dataUrl.split(";")[0].split(":")[1];
+  if (!mimeType || !base64Data) {
+    throw new Error("Invalid source image payload");
+  }
+  const response = await puter.ai.txt2img(ROOMIFY_RENDER_PROMPT, {
+    provider: "gemini",
+    model: "gemini-2.5-flash-image-preview",
+    input_image: base64Data,
+    input_image_mime_type: mimeType,
+    ratio: { w: 1024, h: 1024 },
+  });
+  const rawImageUrl = (response as HTMLImageElement).src ?? null;
+  if(!rawImageUrl) return {
+    renderedImage: null,
+    renderedPath: undefined
+  }
+
+  const renderedImage = rawImageUrl.startsWith('data:') ? rawImageUrl : await fetchAsDataUrl(rawImageUrl);
+
+  return {
+    renderedImage,
+    renderedPath: undefined
+  }
+};
